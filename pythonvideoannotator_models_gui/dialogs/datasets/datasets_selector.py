5dfdec537af1e49f321444b2a8edc8d52a109960
Ricardo Ribeiro <ricardojvr@gmail.com>
2018-01-07 22:15:08 +0000

    .

--- pythonvideoannotator_models_gui/dialogs/datasets/datasets_selector.py
+++ pythonvideoannotator_models_gui/dialogs/datasets/datasets_selector.py
@@ -9,119 +9,148 @@ from pythonvideoannotator_models_gui.dialogs.objects.objects import ObjectsDialo
 
 class DatasetsSelectorDialog(ObjectsDialog):
 
-	def __init__(self, parent_win=None):
-		ObjectsDialog.__init__(self,parent_win=parent_win, title='Datasets selector',)
+    def __init__(self, parent_win=None):
+        ObjectsDialog.__init__(self,parent_win=parent_win, title='Datasets selector',)
 
-		self._datasets = ControlCheckBoxList('Datasets filter')
+        self._datasets = ControlCheckBoxList('Datasets filter')
 
-		self.formset = [('_videos','||','_objects','||','_datasets')]
+        self.formset = [('_videos','||','_objects','||','_datasets')]
 
-		self._objects.changed_event  = self.__update_datasets
-		self._datasets.changed_event = self.__datasets_changed_event
+        self._objects.changed_event  = self.__update_datasets
+        self._datasets.changed_event = self.__datasets_changed_event
 
-		#for video in conf.PROJECT.videos: self += video
+        #for video in conf.PROJECT.videos: self += video
 
-	#####################################################################
-	### PRIVATE FUNCTIONS ###############################################
-	#####################################################################
+    #####################################################################
+    ### PRIVATE FUNCTIONS ###############################################
+    #####################################################################
 
-	def __datasets_changed_event(self): self.datasets_changed_event()
+    def __datasets_changed_event(self): self.datasets_changed_event()
 
-	#####################################################################
-	### EVENTS ##########################################################
-	#####################################################################
+    #####################################################################
+    ### EVENTS ##########################################################
+    #####################################################################
 
-	def datasets_changed_event(self): pass
+    def datasets_changed_event(self): pass
 
-	#####################################################################
-	### FUNCTIONS #######################################################
-	#####################################################################
+    #####################################################################
+    ### FUNCTIONS #######################################################
+    #####################################################################
 
-	# used to update automaticly the name of the videos, objects and paths
-	def refresh(self):
-		ObjectsDialog.refresh(self)
-		self._datasets.refresh()
+    """
+    def save_form(self, data={}, path=None):
+        allparams = self.controls
 
-	def clear(self):
-		ObjectsDialog.clear(self)
-		self._datasets.clear()
-	
-	def __add__(self, other):
-		ObjectsDialog.__add__(self, other)
-		if isinstance(other, Dataset): 	self.__update_datasets()
-		return self
-		
+        if hasattr(self, 'load_order'):
+            for name in self.load_order:
+                param = allparams[name]
+                data[name] = {}
+                param.save_form(data[name])
+        else:
+            for name, param in allparams.items():
+                data[name] = {}
+                param.save_form(data[name])
+        return data
 
-	def __sub__(self, other):
-		ObjectsDialog.__sub__(self, other)
-		if isinstance(other, Dataset): 	self.__update_datasets()
-		return self
-	
+    def load_form(self, data, path=None):
+        allparams = self.controls
 
-	def update_objects(self):
-		"""
-		Update the objects in the list
-		"""
-		ObjectsDialog.update_objects(self)
-		self.__update_datasets()
-	
+        if hasattr(self, 'load_order'):
+            for name in self.load_order:
+                param = allparams[name]
+                if name in data:
+                    param.load_form(data[name])
+        else:
+            for name, param in allparams.items():
+                if name in data:
+                    param.load_form(data[name])
+    """
+
+    # used to update automaticly the name of the videos, objects and paths
+    def refresh(self):
+        ObjectsDialog.refresh(self)
+        self._datasets.refresh()
+
+    def clear(self):
+        ObjectsDialog.clear(self)
+        self._datasets.clear()
+    
+    def __add__(self, other):
+        ObjectsDialog.__add__(self, other)
+        if isinstance(other, Dataset):  self.__update_datasets()
+        return self
+        
+
+    def __sub__(self, other):
+        ObjectsDialog.__sub__(self, other)
+        if isinstance(other, Dataset):  self.__update_datasets()
+        return self
+    
+
+    def update_objects(self):
+        """
+        Update the objects in the list
+        """
+        ObjectsDialog.update_objects(self)
+        self.__update_datasets()
+    
 
 
-	def __update_datasets(self):
-		"""
-		Update the paths in the list
-		"""
-		datasets = [elem for elem, checked in self._datasets.items]
+    def __update_datasets(self):
+        """
+        Update the paths in the list
+        """
+        datasets = [elem for elem, checked in self._datasets.items]
 
-		datasets_list = []
-		for obj, checked in self._objects.items:
-			if not isinstance(obj,Object2D): continue
+        datasets_list = []
+        for obj, checked in self._objects.items:
+            if not isinstance(obj,Object2D): continue
 
-			for dataset in obj.datasets:
-				if hasattr(self, '_datasets_filter') and not self._datasets_filter(dataset): continue
-				
-				if dataset not in datasets_list: datasets_list.append(dataset)
+            for dataset in obj.datasets:
+                if hasattr(self, '_datasets_filter') and not self._datasets_filter(dataset): continue
+                
+                if dataset not in datasets_list: datasets_list.append(dataset)
 
-				if not checked and dataset in datasets:
-					self._datasets -= dataset
-				elif checked and dataset not in datasets:
-					self._datasets += (dataset, False)
+                if not checked and dataset in datasets:
+                    self._datasets -= dataset
+                elif checked and dataset not in datasets:
+                    self._datasets += (dataset, False)
 
-		for dataset in datasets:
-			if dataset not in datasets_list: self._datasets -= dataset
+        for dataset in datasets:
+            if dataset not in datasets_list: self._datasets -= dataset
 
-		
+        
 
-	
+    
 
-	#####################################################################
-	### PROPERTIES ######################################################
-	#####################################################################
+    #####################################################################
+    ### PROPERTIES ######################################################
+    #####################################################################
 
-	@property
-	def datasets(self): return self._datasets.value
+    @property
+    def datasets(self): return self._datasets.value
 
-	@property
-	def selected_data(self):
-		videos 	= self._videos.value
-		datasets= self._datasets.value
+    @property
+    def selected_data(self):
+        videos  = self._videos.value
+        datasets= self._datasets.value
 
-		res 	= []
-		for video in videos:
-			datasets_list = []
-			for obj in video.objects2D:
-				for dataset in obj.datasets:
-					if dataset in datasets:
-						datasets_list.append(dataset)
-			res.append( (video, datasets_list) )
-		return res
-		
-	@property
-	def datasets_filter(self): return self._datasets_filter
-	@datasets_filter.setter
-	def datasets_filter(self, value): self._datasets_filter = value
+        res     = []
+        for video in videos:
+            datasets_list = []
+            for obj in video.objects2D:
+                for dataset in obj.datasets:
+                    if dataset in datasets:
+                        datasets_list.append(dataset)
+            res.append( (video, datasets_list) )
+        return res
+        
+    @property
+    def datasets_filter(self): return self._datasets_filter
+    @datasets_filter.setter
+    def datasets_filter(self, value): self._datasets_filter = value
 
-	
-	
-	
-if __name__ == "__main__":	 pyforms.startApp( DatasetsSelectorDialog )
+    
+    
+    
+if __name__ == "__main__":   pyforms.startApp( DatasetsSelectorDialog )
