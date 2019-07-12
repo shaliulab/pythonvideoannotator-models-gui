3d0d81077b02cc0d02c1147a2af1cbde3ffd8285
Ricardo Ribeiro <ricardojvr@gmail.com>
2019-07-12 13:51:55 +0100

    Update versioning system

--- pythonvideoannotator_models_gui/__init__.py
+++ pythonvideoannotator_models_gui/__init__.py
@@ -1,1 +1,2 @@
 from confapp import conf; conf += 'pythonvideoannotator_models_gui.settings'
+__version__ = "0.6.56"
--- setup.py
+++ setup.py
@@ -2,11 +2,18 @@
 # -*- coding: utf-8 -*-
 
 from setuptools import setup, find_packages
-import re
+import re, os
+
+PACKAGE_PATH = os.path.dirname(os.path.realpath(__file__))
+with open(os.path.join(PACKAGE_PATH, 'pythonvideoannotator_models_gui','__init__.py'), 'r') as fd:
+    content = fd.read()
+    version = re.search(
+        r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', content, re.MULTILINE).group(1)
+
 
 setup(
 	name='Python video annotator - models - gui',
-	version=0.6,
+	version=version,
 	description="""""",
 	author=['Ricardo Ribeiro'],
 	author_email='ricardojvr@gmail.com',
