8718d91fec6928cd61db3bfd8525fdd276688e0c
Ricardo Ribeiro <ricardojvr@gmail.com>
2019-08-14 19:38:28 +0100

    Auto stash before merge of "master" and "origin/master"

--- pythonvideoannotator_models_gui/__init__.py
+++ pythonvideoannotator_models_gui/__init__.py
@@ -1,2 +1,6 @@
 from confapp import conf; conf += 'pythonvideoannotator_models_gui.settings'
+<<<<<<< Updated upstream
 __version__ = "0.6.57"
+=======
+__version__ = "0.7.57"
+>>>>>>> Stashed changes
