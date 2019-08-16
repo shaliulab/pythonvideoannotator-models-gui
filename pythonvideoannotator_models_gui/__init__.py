fb52c4d670d6513579e46a0b2efbd6a61822f1d2
Ricardo Ribeiro <ricardojvr@gmail.com>
2019-08-16 13:33:46 +0100

    Fix issue with version

--- pythonvideoannotator_models_gui/__init__.py
+++ pythonvideoannotator_models_gui/__init__.py
@@ -1,6 +1,2 @@
 from confapp import conf; conf += 'pythonvideoannotator_models_gui.settings'
-<<<<<<< Updated upstream
-__version__ = "0.6.57"
-=======
 __version__ = "0.7.57"
->>>>>>> Stashed changes
